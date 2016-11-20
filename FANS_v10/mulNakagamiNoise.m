2f6d82f6eb1a13475b831793de8c9b5a9918781a
Zhi-Huo XU <xuzhihuo@gmail.com>
2016-11-20 17:44:30 +0800

    Revert "Nonstationary-Speckle-Reduction"
    This reverts commit 0709d00543bfb51b4f9f8dc947f6cf29dbb7514a.

--- FANS_v10/mulNakagamiNoise.m
+++
@@ -1,4 +0,0 @@
-function noisy = mulNakagamiNoise(im,L,seed)
-    s = RandStream('mt19937ar','Seed',seed);
-    noise_int = mean(randn(s, [size(im, 1),size(im, 2),2*L] ).^2,3);
-    noisy = sqrt(noise_int).*im;
--- FoE-Naka/compute_psnr.m
+++
@@ -1,4 +0,0 @@
-function [decibels] = compute_psnr(A,B)
-
-error_diff = A - B;
-decibels = 20*log10(1/(sqrt(mean(mean(error_diff.^2)))));
